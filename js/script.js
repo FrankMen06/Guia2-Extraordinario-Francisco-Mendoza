07edd0c3e92cfa3dade5f146ca16fdeccbb8763c
Frank <100966778+FrankMen06@users.noreply.github.com>
2022-11-10 09:41:21 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,48 @@
-function provisional(){
+function porcentaje(){
+
+    chesse = document.getElementById("chesse").value;
+    if(chesse == onclick){
+        contaChesee++;
+    }
+
+    flan = document.getElementById("flan").value;
+    if(flan == onclick){
+        contaFlan++;
+    }
+
+    helado = document.getElementById("helado").value;
+    if(helado == onclick){
+        contaHelado++;
+    }
+
+    lavaCake = document.getElementById("lavaCake").value;
+    if(lavaCake == onclick){
+        contaLava++;
+    }
+    
+   resultado = document.getElementById("resultado").value;
+
+    total = contaChesee + contaFlan + contaHelado + contaLava;
+    
+    resultado.innerHTML = total;
+
+}
+
+function grande(){
+    img = document.getElementById("cheese").value;
+    img = "style{ width = '400px', heigh='400px'}"
+}
+
+function votar(){
+    let respuesta = window.prompt("Ingrese la contraseña para iniciar (contraseña: votacion2023)");
+
+    if(respuesta == "votacion2023"){
+        window.location.href="../votacion/index.html";
+
+    }else if(!(respuesta == "votacion2023")){
+        alert("contraseña incorrecta...  ingrese contra correcta");
+        return;
+    }
+    
     
 }
